f2ec949f15b506b75227fd5d307b5f0378ec8647
KRMadhusanka99 <Kkrmadhu1999@gmail.com>
2023-03-14 18:24:21 +0530

    version 1 over

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Navbar from "../src/layout/Navbar"
 import Home from "./pages/Home";
 import AddUser from "./users/AddUser"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser"
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Routes>
               <Route exact path="/" element={<Home/>}></Route>
               <Route exact path="/adduser" element={<AddUser/>}></Route>
+              <Route exact path="/edituser/:id" element={<EditUser />} />
+              <Route exact path="/viewuser/:id" element={<ViewUser />} />
           </Routes>
       </Router>
     </div>
