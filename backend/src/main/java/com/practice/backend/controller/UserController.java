a2bc1bb50f914f927eeb35abc60178adf2136142
KRMadhusanka99 <Kkrmadhu1999@gmail.com>
2023-03-14 15:15:21 +0530

    Connect frontend and backend axios and crossorigin

--- backend/src/main/java/com/practice/backend/controller/UserController.java
+++ backend/src/main/java/com/practice/backend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.practice.backend.controller;
 import com.practice.backend.model.User;
 import com.practice.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const Home = () =>{
+    const [users,setUsers] = useState([]);
+
+    useEffect(()=>{
+        loadUsers();
+    },[]);
+
+    const loadUsers=async ()=>{
+        const result = await axios.get("http://localhost:8080/users");
+        //console.log(result.data);
+        setUsers(result.data);
+    }
+
     return(
         <div className="container">
             <div className="py-4">
@@ -8,29 +21,21 @@ const Home = () =>{
                     <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">User Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                        {users.map((users,index)=>(
+                            <tr>
+                                <th scope="row" key={index}>{index+1}</th>
+                                <td>{users.name}</td>
+                                <td>{users.username}</td>
+                                <td>{users.email}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
